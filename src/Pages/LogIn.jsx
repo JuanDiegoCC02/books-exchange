90e94395a19b65e76217fc58177665d6f83b8689
JuanDiegoCC02 <dcorella2324@gmail.com>
2025-10-02 14:29:09 -0600

    Actualizaciones del  Proyecto

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
 
 import FormLogIn from '../Components/FormLogIn'
-<<<<<<< HEAD
 import NavPages from '../Components/NavPages'
-=======
-import HeaderPages from '../Components/HeaderPages'
->>>>>>> 15af5fc8b96b86d19c72426aa819cccc68b7cfd5
 import FooterPages from '../Components/FooterPages'
 
 function LogIn() {
   return (
     <div>
-<<<<<<< HEAD
+
       <nav>
         <NavPages />
       </nav>
-=======
-      <header>
-        <HeaderPages />
-      </header>
->>>>>>> 15af5fc8b96b86d19c72426aa819cccc68b7cfd5
       <main>
         <FormLogIn />
       </main>
