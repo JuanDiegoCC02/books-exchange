2603e1facbc5474334579e2e610933b89d01ce96
JuanDiegoCC02 <dcorella2324@gmail.com>
2026-02-03 10:30:22 -0600

    Updating newStyles & newStructure ContactUs

--- src/Components/ContactMain.jsx
+++ src/Components/ContactMain.jsx
@@ -9,15 +9,18 @@ function ContactMain() {
     <div className='containerTitleContacUs'>
         <h1 className='titleContacUs'>Contact Us</h1>
     </div>
-    <div className='fullDescripContainer'>
-    <div className='ContainerDataPag'>
-        <p className='DescriptionContacUs'>Our Book Exchange platform was created to promote reading, learning, and the reuse of books, giving them a second
-             life and connecting readers from different communities. Here you can discover new titles, share your own books, and create a collaborative space where reading is accessible to all.
-         </p>
-          <p className='InfoTYPag'>If you have questions, comments, or suggestions, please don't hesitate to contact us. Your participation is what keeps this reading community alive. 
-       <br /> Thank you for visiting!
-     </p>
 
+    <div className='fullDescripContainer'> 
+    <div className='ContainerDataPag'>
+        <div className='containerDIContacUs'>
+          <p className='DescriptionContacUs'>Our Book Exchange platform was created to promote reading, learning, and the reuse of books, giving them a second
+             life and connecting readers from different communities. Here you can discover new titles, share your own books, and create a collaborative space where reading is accessible to all.
+          </p>
+          <p className='InfoTYPag'>If you have questions, comments, or suggestions, please don't hesitate to contact us. Your participation is what keeps this reading community alive. 
+             <br /> Thank you for visiting!
+          </p>
+         </div>
+      
      <div className='ContainerContactUs'>
     <div className='ContainerData'>
         <h4 className='titleHrContacUs'>Days of attention</h4>
